4d8ae33542abb9fb0fb91e536a7c15f287bec323
Austin-Fulbright <austinfulbright@gmail.com>
2025-03-13 20:00:54 -0400

    added block game

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use wasm_bindgen::prelude::*;
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, KeyboardEvent};
+
+// Game state
+#[wasm_bindgen]
+pub struct Game {
+    context: CanvasRenderingContext2d,
+    x: f64,
+    y: f64,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+impl Game {
+    #[wasm_bindgen(constructor)]
+    pub fn new(canvas: HtmlCanvasElement) -> Result<Game, JsValue> {
+        let context = canvas
+            .get_context("2d")?
+            .unwrap()
+            .dyn_into::<CanvasRenderingContext2d>()?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        Ok(Game { context, x: 50.0, y: 50.0 })
+    }
+
+    pub fn update(&mut self) {
+        self.context.clear_rect(0.0, 0.0, 500.0, 500.0);
+        self.context.set_fill_style(&JsValue::from_str("black"));
+        self.context.fill_rect(self.x, self.y, 30.0, 30.0);
+    }
+
+    pub fn move_square(&mut self, direction: &str) {
+        match direction {
+            "ArrowUp" => self.y -= 5.0,
+            "ArrowDown" => self.y += 5.0,
+            "ArrowLeft" => self.x -= 5.0,
+            "ArrowRight" => self.x += 5.0,
+            _ => {}
+        }
     }
 }
